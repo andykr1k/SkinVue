74368ffb23a1b067cb72ab6664391ee257a1ada7
andykr1k <69021102+andykr1k@users.noreply.github.com>
2024-04-28 04:22:39 -0700

    Added Dashboard features

--- Backend/api/index.py
+++ Backend/api/index.py
@@ -9,55 +9,55 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-@app.route('/predict/<path>')
-def predict(path):
-    url = "https://whqnperemsymnmpfsoyi.supabase.co/storage/v1/object/public/pictures/"
+# @app.route('/predict/<path>')
+# def predict(path):
+#     url = "https://whqnperemsymnmpfsoyi.supabase.co/storage/v1/object/public/pictures/"
 
-    image_path = url + path
+#     image_path = url + path
 
-    # image = preprocessImage(file)
+#     # image = preprocessImage(file)
 
-    # prediction = CNN(image)
+#     # prediction = CNN(image)
 
-    # Update DB with Prediction
+#     # Update DB with Prediction
 
-    return image_path
+#     return image_path
 
 
-def preprocessImage(image):
-    with Image.open(image) as img:
-        img_gray = img.convert('L')
-        img_resized = img_gray.resize((28, 28))
-        image_array = np.array(img_resized, dtype=np.float32) / 255.0
-    return image_array
+# def preprocessImage(image):
+#     with Image.open(image) as img:
+#         img_gray = img.convert('L')
+#         img_resized = img_gray.resize((28, 28))
+#         image_array = np.array(img_resized, dtype=np.float32) / 255.0
+#     return image_array
 
-def CNN(image):
-    model = Sequential([
-        layers.Conv2D(16, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-        layers.MaxPooling2D(),
-        layers.BatchNormalization(),
-        layers.Conv2D(32, (3, 3), activation='relu'),
-        layers.MaxPooling2D(),
-        layers.BatchNormalization(),
-        layers.Conv2D(32, (3, 3), activation='relu'),
-        layers.MaxPooling2D(),
-        layers.BatchNormalization(),
-        layers.Flatten(),
-        layers.Dense(40, activation='relu'),
-        layers.Dropout(0.6),
-        layers.Dense(7, activation='softmax')
-    ])
+# def CNN(image):
+#     model = Sequential([
+#         layers.Conv2D(16, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+#         layers.MaxPooling2D(),
+#         layers.BatchNormalization(),
+#         layers.Conv2D(32, (3, 3), activation='relu'),
+#         layers.MaxPooling2D(),
+#         layers.BatchNormalization(),
+#         layers.Conv2D(32, (3, 3), activation='relu'),
+#         layers.MaxPooling2D(),
+#         layers.BatchNormalization(),
+#         layers.Flatten(),
+#         layers.Dense(40, activation='relu'),
+#         layers.Dropout(0.6),
+#         layers.Dense(7, activation='softmax')
+#     ])
 
-    model.load_weights('../weights/train_1.h5')
+#     model.load_weights('../weights/train_1.h5')
     
-    model.compile(
-        optimizer='adam',
-        loss='categorical_crossentropy',
-        metrics=['accuracy']
-    )
+#     model.compile(
+#         optimizer='adam',
+#         loss='categorical_crossentropy',
+#         metrics=['accuracy']
+#     )
 
-    image = image.reshape(1, 28, 28, 1)
-    predictions = model.predict(image)
-    arr = np.round(predictions)
-    print(arr)
-    return arr
+#     image = image.reshape(1, 28, 28, 1)
+#     predictions = model.predict(image)
+#     arr = np.round(predictions)
+#     print(arr)
+#     return arr
